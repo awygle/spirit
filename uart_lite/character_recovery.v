ae80bed3a2e3c770f52a4f3992e79d2e8c7407c7
Andrew Wygle <awygle@gmail.com>
2018-02-26 22:43:51 -0800

    Combined bit and character recovery. Switched to a center-sampled approach.

--- uart_lite/character_recovery.v
+++ uart_lite/character_recovery.v
@@ -3,13 +3,13 @@
 module character_recovery
 #(
 	parameter OVERSAMPLING = 16,
+	parameter GLITCH_LENGTH = 1,
 	parameter DATA_BITS = 8
 )
 (
 	input rst_i,
 	input clk_i,
 	input rx_i,
-	input valid_i,
 	output reg [DATA_BITS-1:0] char_o,
 	output reg valid_o
 );
@@ -23,62 +23,58 @@ reg [DATASIZE-1:0] index;
 reg [1:0] state;
 
 localparam IDLE = 2'b00;
-localparam STARTED = 2'b01;
-localparam CAPTURED = 2'b10;
+localparam STARTING = 2'b01;
+localparam STARTED = 2'b10;
+localparam CAPTURED = 2'b11;
 
-wire counter_full;
-assign counter_full = counter == {COUNTSIZE{1'b1}};
+wire counter_empty;
+assign counter_empty = counter == {COUNTSIZE{1'b0}};
+
+reg past_rx;
 
 always @(posedge clk_i) begin
 	if (rst_i) begin
 		valid_o <= 1'b0;
 		state <= IDLE;
-		// No need to reset char_o, counter
+		past_rx <= 1'b0;
+		counter <= {COUNTSIZE{1'b0}};
+		// No need to reset char_o
 	end
 	else begin
+		past_rx <= rx_i;
 		case (state)
 			IDLE: begin
 				valid_o <= 1'b0;
-				if (valid_i && rx_i) begin
-					state <= STARTED;
+				if (counter_empty && !past_rx && rx_i) begin
+					state <= STARTING;
 					index <= {DATASIZE{1'b0}};
-					counter <= {COUNTSIZE{1'b0}};
+					counter <= (OVERSAMPLING >> 1) - 1'b1;
+				end
+			end
+			STARTING: begin
+				counter <= counter - 1'b1;
+				if (counter_empty) begin
+					state <= rx_i ? STARTED : IDLE;
+					counter <= OVERSAMPLING-1;
 				end
 			end
 			STARTED: begin
-				counter <= counter + 1'b1;
-				case ({counter_full, valid_i}) 
-					2'b11: begin // counter full + valid - advance
-						char_o[index] <= rx_i;
-						index <= index + 1'b1;
-						if (index == DATA_BITS-1) state <= CAPTURED;
-					end
-					2'b10, 2'b01: begin // not full but valid or not valid but full - error
-						state <= IDLE;
-					end
-					2'b00: begin // not full or valid - continue
-						;
-					end
-				endcase
+				counter <= counter - 1'b1;
+				if (counter_empty) begin
+					char_o[index] <= rx_i;
+					index <= index + 1'b1;
+					if (index == DATA_BITS-1) state <= CAPTURED;
+					counter <= OVERSAMPLING-1;
+				end
 			end
 			CAPTURED: begin
-				counter <= counter + 1'b1;
-				case ({counter_full, valid_i})
-					2'b11: begin 
-						valid_o <= ~rx_i; // stop bit must be low
-						state <= IDLE;
-					end
-					2'b10, 2'b01: begin // error
-						state <= IDLE;
-					end
-					2'b00: begin // continue
-						;
-					end
-				endcase
-			end
-			default: begin
-				// impossible
-				state <= IDLE;
+				counter <= counter - 1'b1;
+				if (counter_empty) begin
+					valid_o <= ~rx_i; // stop bit must be low
+					state <= IDLE;
+					// wait out the rest of the bit period before restart
+					counter <= (OVERSAMPLING >> 1) - 1'b1 + (OVERSAMPLING & 1'b1);
+				end
 			end
 		endcase
 	end
--- uart_lite/verification/character_recovery_formal.v
+++ uart_lite/verification/character_recovery_formal.v
@@ -2,8 +2,6 @@
 
 module character_recovery_formal ();
 
-wire rx_bit;
-wire bit_valid;
 wire [7:0] char;
 wire valid;
 reg rst;
@@ -18,17 +16,17 @@ always @(posedge clk) begin
 end
 
 // Property: all accepted inputs well-formed, no invalid inputs accepted
-reg [9:0] f_hold;
+reg [151:0] f_hold;
+integer f_i;
 always @(posedge clk) begin
-	if (bit_valid) begin
-		f_hold[9] <= rx_bit;
-		f_hold[0:8] <= f_hold[1:9];
-	end
+	f_hold[151] <= rx;
+	f_hold[0:150] <= f_hold[1:151];
 	if (f_reset_in_past) begin
 		if (!rst && valid) begin
-			assert(f_hold[0] == 1'b1);
-			assert(f_hold[9] == 1'b0);
-			assert(f_hold[1:8] == char);
+			assert(f_hold[7]);
+			assert(~f_hold[151]);
+			for (f_i = 16; f_i < 136; f_i = f_i + 16)
+				assert(f_hold[f_i+7] == char[(f_i/16)-1]);
 		end
 	end
 end
@@ -38,15 +36,18 @@ reg [7:0] f_counter;
 initial f_counter = 8'd159;
 always @(posedge clk) begin
 	if (f_counter < 8'd159) f_counter <= f_counter + 1'b1;
-	if (rst) begin
-		f_counter <= 8'd159;
-	end
 	if (f_reset_in_past) begin
 		assume(f_counter <= 8'd159);
 		if (valid) begin
 			assert(f_counter == 8'd159);
 			f_counter <= 8'd0;
 		end
+	end
+	// This is below the check because if the core asserts valid on the same
+	// cycle that the controller asserts reset, we still consider the property
+	// to hold.
+	if (rst) begin
+		f_counter <= 8'd159;
 	end
 end
 
@@ -73,18 +74,6 @@ always @(posedge clk) begin
 	end
 end
 
-bit_recovery 
-#(
-	.OVERSAMPLING(16)
-) bit_recovery
-(
-	.rst_i(rst),
-	.clk_i(clk),
-	.rx_i(rx),
-	.rx_bit_o(rx_bit),
-	.valid_o(bit_valid)
-);
-
 character_recovery
 #(
 	.OVERSAMPLING(16),
@@ -93,8 +82,7 @@ character_recovery
 (
 	.rst_i(rst),
 	.clk_i(clk),
-	.rx_i(rx_bit),
-	.valid_i(bit_valid),
+	.rx_i(rx),
 	.char_o(char),
 	.valid_o(valid)
 );
